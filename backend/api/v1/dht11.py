e82a2522d1c1a8fbfd52d50ed144a0dd17eb915f
BERKANI Ilyas <dev.berkail@gmail.com>
2025-11-01 23:51:16 +0100

    feature: add dht11 readings POST endpoint

--- backend/api/v1/dht11.py
+++ backend/api/v1/dht11.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from fastapi_pagination.limit_offset import LimitOffsetPage
 from fastapi_pagination.ext.sqlmodel import paginate
 from sqlmodel import asc, select
+from schemas.dht11_schemas import DHT11ReadingData
 from models.dht11_models import DHT11Reading
 from core.db import DBSession
 
@@ -23,8 +24,9 @@ async def get_test():
 def get_readings(
     session: DBSession,
     start: datetime | None = Query(None, description="Start timestamp"),
-    end: datetime | None = Query(None, description="End timestamp")
-):
+    end: datetime | None = Query(None, description="End timestamp"),
+) -> LimitOffsetPage[DHT11Reading]:
+    
     query = select(DHT11Reading)
 
     if start is not None:
@@ -35,3 +37,20 @@ def get_readings(
     query = query.order_by(asc(DHT11Reading.timestamp))
     
     return paginate(session, query)
+
+@router.post("/readings", response_model=DHT11Reading, status_code=201)
+def create_reading(
+    session: DBSession,
+    reading_data: DHT11ReadingData,
+) -> DHT11Reading:
+
+    new_reading = DHT11Reading(
+        temperature=reading_data.temperature,
+        humidity=reading_data.humidity,
+    )
+
+    session.add(new_reading)
+    session.commit()
+    session.refresh(new_reading)
+
+    return new_reading
