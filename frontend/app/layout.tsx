80e958506e1c258d3254b54fb5aef3ed8e2b6911
BERKANI Ilyas <dev.berkail@gmail.com>
2025-11-13 19:40:51 +0100

    Revert "feature : add auth N"
    This reverts commit 3779b379a1ba69ad76eef364769eccbb23e784dd.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "@/components/AuthProviderClient";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -24,9 +23,7 @@ export default function RootLayout({
       <body
         className={`${roboto.variable} antialiased bg-gray-50 min-h-screen`}
       >
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        {children}
       </body>
     </html>
   );
